5774816d5027fba3ee8286637d569701ea3827b5
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2022-11-03 19:14:45 +0100

    update enum declaration

--- index.js
+++ index.js
@@ -22,8 +22,15 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-
+    console.log('Request body: ', req.body);
     const {operation_type, x, y} = req.body;
+
+    // This object serves as the enum for 'operation_type'
+    const OperationEnum = {
+        'Addition': 1,
+        'Subtraction': 2,
+        'Multiplication': 3
+    }
 
     // This function parses the operation_type string
     const parseOpType = opType => {
@@ -32,59 +39,77 @@ app.post('/', (req, res) => {
         
         let matchedType = '';
 
-        for(let possibleOpType of possibleOpTypes){
-        
-          if(opType.toLowerCase().indexOf(possibleOpType) > -1){
+        if(typeof opType === 'string'){
+            for(let possibleOpType of possibleOpTypes){
+            
+            if( opType.toLowerCase().indexOf(possibleOpType) > -1){
 
-            switch(possibleOpType){
-                case 'add':
-                    matchedType = 'addition';
+                switch(possibleOpType){
+                    case 'add':
+                        matchedType = OperationEnum.Addition;
+                        break;
+                    case 'subtract':
+                        matchedType = OperationEnum.Subtraction;
+                        break;
+                    case 'multiply':
+                        matchedType = OperationEnum.Multiplication;
                     break;
-                case 'subtract':
-                    matchedType = 'subtraction';
-                    break;
-                case 'multiply':
-                    matchedType = 'multiplication';
+                }
+
+            }  
+            
+            if(matchedType !== ''){
                 break;
             }
-
-          }  
-          
-          if(matchedType !== ''){
-            break;
-          }
+            }
+        }
+         // Get the matching opType from OperationEnum
+        if (typeof opType === 'number'){
+            console.log(0 === OperationEnum.Addition);
+            console.log('check vall',opType, OperationEnum.Addition);
+            switch(opType){
+                case OperationEnum.Addition:
+                    matchedType = OperationEnum.Addition;
+                    break;
+                case OperationEnum.Subtraction:
+                    matchedType = OperationEnum.Subtraction;
+                    break;
+                case OperationEnum.Multiplication:
+                    matchedType = OperationEnum.Multiplication  ;
+                break;
+                default:
+                    break;
+            }
         }
         
         return matchedType;
     }
 
     const matchedOpType = parseOpType(operation_type);
-
-    // This object serves as the enum for 'operation_type'
-    const OperationType = {
-        'Addition': 'addition',
-        'Subtraction': 'subtraction',
-        'Multiplication': 'multiplication'
-    }
-
+    console.log('mop', matchedOpType);
     let result = 0;
+
 
     switch(matchedOpType){
 
-        case OperationType.Addition:
+        case OperationEnum.Addition:
             result = parseInt(x) + parseInt(y);
             break;
-        case OperationType.Subtraction:
+        case OperationEnum.Subtraction:
             result = parseInt(x) - parseInt(y);
             break;
-        case OperationType.Multiplication:
+        case OperationEnum.Multiplication:
             result = parseInt(x) * parseInt(y);
             break;
         default:
             break;
     }
+    
+    console.log('Check variables:', result, matchedOpType, x, y);
 
-    return res.status(200).json({ "slackUsername": "kazeemkadiri", result, "operation_type": matchedOpType }).end();
+
+
+    return res.status(200).json({ "slackUsername": "kazeemkadiri", "result": result, "operation_type": matchedOpType }).end();
     
 });
 
