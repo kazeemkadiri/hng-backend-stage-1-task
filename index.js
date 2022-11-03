0b99990f7ae85532e4fb81d91697887e54dead10
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2022-11-03 15:33:09 +0100

    task 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
-const server = http.createServer((req, res, next) => {
+
+const server = http.createServer((req, res) => {
     
     if(req.method === 'GET' ){
         
@@ -8,13 +9,66 @@ const server = http.createServer((req, res, next) => {
             
             return res.end(
                 JSON.stringify({ 
-                    "slackUsername": 'Kezzy', 
+                    "slackUsername": 'kazeemkadiri', 
                     "backend": true, 
                     "age": 29, 
                     "bio": "Software Engineer (Backend)" 
                 })
             );
         
+        }
+
+    }else if(req.method === 'POST' ){
+
+        if(req.url === '/'){
+            const chunks = [];
+
+            req.on("data", (chunk) => {
+
+                chunks.push(chunk);
+
+            });
+
+            req.on("end", () => {
+                
+                const data = Buffer.concat(chunks);
+                
+                const stringData = data.toString();
+
+                const requestBody = JSON.parse(stringData);
+
+                const {operation_type, x, y} = requestBody;
+
+                // This object serves as the enum for 'operation_type'
+                const OperationType = {
+                    'Addition': 'addition',
+                    'Subtraction': 'subtraction',
+                    'Multiplication': 'multiplication'
+                }
+        
+                let result = 0;
+
+                switch(operation_type){
+                    case OperationType.Addition:
+                        result = parseInt(x) + parseInt(y);
+                        break;
+                    case OperationType.Subtraction:
+                        result = parseInt(x) - parseInt(y);
+                        break;
+                    case OperationType.Multiplication:
+                        result = parseInt(x) * parseInt(y);
+                        break;
+                    default:
+                        break;
+                }
+
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                
+                return res.end(
+                    JSON.stringify({ "slackUsername": "kazeemkadiri", result, "operation_type": operation_type })
+                );
+            });
+    
         }
 
     }
